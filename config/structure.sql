6d846d9c1bd4a3f61a04f82a62b63e752a283f33
SamHaggans <sam@samhaggans.com>
2021-08-02 05:06:30 -0600

    Structural changes

--- config/structure.sql
+++ config/structure.sql
@@ -1,5 +1,8 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (id int auto_increment primary key, username varchar(200), email varchar(200), passwordHash varchar(200), encryptedKey varchar(200));
 
+DROP TABLE IF EXISTS logins;
 CREATE TABLE logins (id int auto_increment primary key, userid varchar(200), title varchar(200), username varchar(200), password varchar(200));
 
-CREATE TABLE notes (id int auto_increment primary key, userid varchar(200), title varchar(200), content varchar(200000));
+DROP TABLE IF EXISTS notes;
+CREATE TABLE notes (id int auto_increment primary key, userid varchar(200), title varchar(200), content varchar(10000));
